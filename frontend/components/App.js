82cb5e3dfd4c2a2906cb828661d10e9eb8f63c3e
Devon McGrath <devonmcg1213@gmail.com>
2024-08-09 04:04:27 -0400

    Finally completed the Sprint 9 module 2 project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import TodoList from './TodoList';
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = () => {
+  return axios.get('http://localhost:9000/api/todos').then(res => res).catch(err => console.err(err));
+}
+
+let id = 0;
+const getId = () => id++;
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    URL(this.state.name).then(res => {
+      this.setState({ ...this.state, todos: res.data.data })
+    })
+  }
+
+  add = (name) => {
+    this.setState({
+      ...this.state, todos: this.state.todos.concat({ id: getId(), completed: false, name })
+    })
+  }
+
+  onClick = (itemId) => {
+    this.setState({
+      ...this.state, todos: this.state.todos.map(todo => {
+        if (itemId === todo.id) return {...todo, completed: !todo.completed}
+        return todo
+      })
+    })
+  }
   render() {
-    return null
+    return (
+      <div>
+        <TodoList todos={this.state.todos} add={this.add} onClick={this.onClick}/> 
+      </div>
+    )
   }
 }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    const { id, name, completed } = this.props.todo
+    const { onClick } = this.props
+    return (
+      <div onClick={() => onClick(id)}>
+        {name} {completed && '✓'}
+      </div>
+    )
   }
 }
